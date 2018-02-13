e41fb8c229642691ec6cdefc769b5eee88fb0104
Arne Peine <arnepeine@gmail.com>
2018-02-13 08:44:56 +0100

    Update apache2-calculator.py

--- apache2-calculator.py
+++ apache2-calculator.py
@@ -2,9 +2,7 @@
 # 02/2018 / Arne Peine
 # APACHE II ("Acute Physiology And Chronic Health Evaluation II") is a severity-of-disease classification system (Knaus et al., 1985), one of several ICU scoring systems. It is applied within 24 hours of admission of a patient to an intensive care unit (ICU): an integer score from 0 to 71 is computed based on several measurements; higher scores correspond to more severe disease and a higher risk of death. The first APACHE model was presented by Knaus et al. in 1981.
 # Note: The data used should be from the initial 24 hours in the ICU, worst value should be used.
-# Reference: Reference. Knaus WA Draper EA et al. APACHE II: A severity of disease classification system. Critical Care Medicine. 1985
-
-
+# Reference: Knaus WA Draper EA et al. APACHE II: A severity of disease classification system. Critical Care Medicine. 1985
 
 # 1. Required Variables and explanations
 
